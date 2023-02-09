e31205938940c655c4277ae6cf327fefb4c6a11a
Andresito <alumno.577931@ies-azarquiel.es>
2023-02-09 13:13:26 +0100

    Creamos usuario y lo devuelve por id. Min del 1:17 al 1:26 aprox.

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.models.Usuario;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,44 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="prueba")
-    public List<String> prueba(){
-        return List.of("prueba","prueba2","prueba3");
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Andrés");
+        usuario.setApellidos("Xandri");
+        usuario.setEmail("alumno.577931@ies-azarquiel.es");
+        usuario.setTelefono("673692304");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario45")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Andrés");
+        usuario.setApellidos("Xandri");
+        usuario.setEmail("alumno.577931@ies-azarquiel.es");
+        usuario.setTelefono("673692304");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario343")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Andrés");
+        usuario.setApellidos("Xandri");
+        usuario.setEmail("alumno.577931@ies-azarquiel.es");
+        usuario.setTelefono("673692304");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario123")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Andrés");
+        usuario.setApellidos("Xandri");
+        usuario.setEmail("alumno.577931@ies-azarquiel.es");
+        usuario.setTelefono("673692304");
+        return usuario;
     }
 }
