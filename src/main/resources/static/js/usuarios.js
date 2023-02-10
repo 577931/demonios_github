18e7bdfc5ed0a8b325eb1d1041111c14607e3c3e
Andresito <alumno.577931@ies-azarquiel.es>
2023-02-10 09:09:34 +0100

    COMMIT DEL RESTO DE ARCHIVOS

---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,19 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  alert("Esto es una prueba js");
+  $('#usuarios').DataTable();
+});
+
+async function cargaUsuarios(){
+
+  const request = await fetch('usuario/234', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+  const usuarios = await request.json();
+
+  console.log(usuarios);
+}
