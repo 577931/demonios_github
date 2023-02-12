e226fd7663cc82dacee9d66bc9951e134cc5e3cb
Andresito <alumno.577931@ies-azarquiel.es>
2023-02-12 18:29:50 +0100

    Back envía usuarios. Front muestra la lista sin acceder a bd. Corrección en HTML de la llamada al script usuarios. Min 1:55 aprox.

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,6 +21,37 @@ public class UsuarioController {
         usuario.setEmail("alumno.577931@ies-azarquiel.es");
         usuario.setTelefono("673692304");
         return usuario;
+    }
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("Andrés");
+        usuario.setApellidos("Xandri");
+        usuario.setEmail("alumno.577931@ies-azarquiel.es");
+        usuario.setTelefono("673692304");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2L);
+        usuario2.setNombre("Pepe");
+        usuario2.setApellidos("Cépedes");
+        usuario2.setEmail("alumno.182736@ies-azarquiel.es");
+        usuario2.setTelefono("689674835");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(3L);
+        usuario3.setNombre("Marina");
+        usuario3.setApellidos("Landia");
+        usuario3.setEmail("alumno.537465@ies-azarquiel.es");
+        usuario3.setTelefono("753847210");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+
+        return usuarios;
     }
 
     @RequestMapping(value = "usuario45")
