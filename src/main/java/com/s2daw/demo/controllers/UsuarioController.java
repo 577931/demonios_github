025e39558f1c6b8e808b6098810ba0df4d316241
Andresito <alumno.577931@ies-azarquiel.es>
2023-02-13 13:26:57 +0100

    Añadimos el borrado. Min 2:41 aprox.

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -6,6 +6,7 @@ import com.s2daw.demo.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -17,7 +18,7 @@ public class UsuarioController {
     @Autowired
     private UsuarioDao usuarioDao;
 
-    @RequestMapping(value = "usuario/{id}")
+    @RequestMapping(value = "api/usuario/{id}", method = RequestMethod.GET)
     public Usuario getUsuarios(@PathVariable Long id){
         Usuario usuario = new Usuario();
         usuario.setId(id);
@@ -28,13 +29,13 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "usuarios")
+    @RequestMapping(value = "api/usuarios")
     public List<Usuario> getUsuarios(){
         return usuarioDao.getUsuarios();
     }
 
-    @RequestMapping(value = "usuario45")
-    public Usuario editar(){
+    @RequestMapping(value = "api/usuario45")
+    public Usuario editar() {
         Usuario usuario = new Usuario();
         usuario.setNombre("Andrés");
         usuario.setApellidos("Xandri");
@@ -43,17 +44,12 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "usuario343")
-    public Usuario eliminar(){
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Andrés");
-        usuario.setApellidos("Xandri");
-        usuario.setEmail("alumno.577931@ies-azarquiel.es");
-        usuario.setTelefono("673692304");
-        return usuario;
+    @RequestMapping(value="api/eliminar/{id}",method= RequestMethod.DELETE)
+    public void eliminarUsuario(@PathVariable Long id){
+        usuarioDao.eliminarUsuario(id);
     }
 
-    @RequestMapping(value = "usuario123")
+    @RequestMapping(value = "api/usuario123")
     public Usuario buscar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Andrés");
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 async function cargaUsuarios() {
 
-  const request = await fetch('usuarios', {
+  const request = await fetch('api/usuarios', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -16,17 +16,32 @@ async function cargaUsuarios() {
   });
   const usuarios = await request.json();
 
-    let listadoHTML = '';
+   let listadoHTML = '';
    for (let usuario of usuarios) {
+   let botonEliminar = '<a href="#" onclick="eliminarUsuario('+ usuario.id +')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
    let usuarioHTML = '<tr><td>'+ usuario.id +'</td><td>'+ usuario.nombre +' '+ usuario.apellidos +'</td><td>'
                     + usuario.email +'</td><td>'+ usuario.telefono +
-                    '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+                    '</td><td>'+ botonEliminar +'</td></tr>';
    listadoHTML += usuarioHTML;
    }
 
   console.log(usuarios);
 
-
-
   document.querySelector('#usuarios tbody').outerHTML = listadoHTML;
+
+  async function eliminarUsuario(id){
+
+  if (!confirm('¿Desea eliminar este usuario?')) {
+    return;
+  }
+
+  const request = await fetch('api/usuarios' + id, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+    location.reload()
+  }
 }
