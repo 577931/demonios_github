2c11ba10e159c1570f265354ce924468da60af77
Andresito <alumno.577931@ies-azarquiel.es>
2023-02-14 13:16:12 +0100

    Añadimos opción de registrar de usuario, se corrige usuario.java GeneratedValue. Corregimos telefono en BD. Min 3:07 aprox

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -18,11 +18,12 @@ async function cargaUsuarios() {
 
    let listadoHTML = '';
    for (let usuario of usuarios) {
+      let telefonoTexto=usuario.telefono == null ? '-':usuario.telefono
       let botonEliminar ='<a onclick="eliminarUsuario('+ usuario.id +')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>'
       let usuarioHTML= '<tr><td>'+usuario.id+
                        '<td>'+usuario.nombre+
                        '</td><td>'+usuario.email+
-                       '</td><td>'+usuario.telefono+
+                       '</td><td>'+telefonoTexto+
                        '</td><td>'+botonEliminar+'</td><tr>';
    listadoHTML += usuarioHTML;
    }
