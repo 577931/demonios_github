f8b67e9eaddbd4acc50b216bbe212c82b4636d97
Andresito <alumno.577931@ies-azarquiel.es>
2023-02-16 10:46:00 +0100

    Aplicamos función hash a las contraseñas con Argon2. Min 3:39 aprox

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -16,8 +16,7 @@ async function iniciarSesion(){
     },
     body:JSON.stringify(datos)
   });
-  // No me gusta que devuelva String , cambio a JSON
-  const respuesta = await response.json();
-  if (respuesta.success == 'OK') window.location.href='usuarios.html';
-  else alert(' Credenciales erróneas, intente de nuevo');
+    const respuesta = await response.json();
+    if (respuesta.success == 'OK') window.location.href='usuarios.html';
+    else alert(' Credenciales erróneas, intente de nuevo');
 }
